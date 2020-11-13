19471a043f6e9a4abf4b4b95358a97bbf635b5af
Guillaume GULLI <guillaume.gulli@isen.yncrea.fr>
2020-11-13 09:39:46 +0100

    Exo3 fin

---
+++ Exo3/Exo3/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	char nom[20], prenom[20], sexe;
+	printf("Nom : ");
+	scanf_s("%s", nom, (unsigned)_countof(nom));
+	printf("Prenom : ");
+	scanf_s("%s", prenom, (unsigned)_countof(prenom));
+	printf("Sexe : ");
+	sexe = _getch();
+	if (sexe == 'H') { printf("Monsieur "); }
+	else { printf("Madame "); }
+	printf("%s %s", prenom, nom);
+}
