963f01e913f5a3b5e0e58fd837478406d81c9311
Guillaume GULLI <guillaume.gulli@isen.yncrea.fr>
2020-11-20 13:42:42 +0100

    Exo6 fin

---
+++ Exo6/Exo6/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "fr-FR");
+
+	int valeur;
+	int tableau[100];
+	int* Pointeur;
+
+	time_t t;
+	srand((unsigned)time(&t));
+
+	for (int i = 0; i < 100; i++) {
+		tableau[i] = rand() % 20;
+	}
+
+	Pointeur = &tableau[0];
+
+	printf("\nVeuillez entrer la valeur recherchée entre 0 et 20 :");
+	scanf_s("%d", &valeur);
+
+	printf("La valeur %d a été trouvée en : ", valeur);
+	for (int i = 0; i < 100; i++) {
+		if (*Pointeur == valeur) {
+			printf(" %d ", i);
+		}
+		Pointeur++;
+	}
+
+}
